7fa15084c82a93e485878683c520f6d5ea1624ce
Julen.C <julenchueca99@gmail.com>
2020-02-17 11:00:05 +0100

    mapa 3

---
+++ Grecia_Projets_Git_01/Assets/Scripts/Interactuable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactuable : MonoBehaviour
+{
+
+    public GameObject menu;
+
+    private void OnMouseDown()
+    {
+        menu.SetActive(true);
+    }
+
+   
+
+    
+
+}
